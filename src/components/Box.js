5487903d9d01fe59d495a9ff3d814c3189caf1f2
Steven <stevengf14@gmail.com>
2024-04-01 15:32:58 -0500

    react basics

---
+++ src/components/Box.js
@@ -0,0 +1,11 @@
+import "./Box.css";
+
+function Box() {
+  return (
+    <div className="Box">
+      <div className="color-container"></div>
+    </div>
+  );
+}
+
+export default Box;
---
+++ src/components/ButtonBox.js
@@ -0,0 +1,12 @@
+import "./ButtonBox.css";
+
+function ButtonBox() {
+  return (
+    <div className="ButtonBox">
+      <button>Red</button>
+      <button>Green</button>
+    </div>
+  );
+}
+
+export default ButtonBox;
