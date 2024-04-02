5c068b3029edf5f2be1b785224f05e79d6fae0c9
Steven <stevengf14@gmail.com>
2024-04-02 13:36:46 -0500

    using states

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,11 +1,36 @@
+import React, { Component } from "react";
 import "./Box.css";
+import ColorAppStore from "../stores/ColorAppStore";
+class Box extends Component() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: ColorAppStore.getActiveColor(),
+    };
+  }
 
-function Box() {
-  return (
+  componentDidMount() {
+    ColorAppStore.on("storeUpdated", this.updateColor);
+  }
+
+  componentWillUnmount() {
+    ColorAppStore.removeListener("storeUpdated", this.updateColor);
+  }
+
+  updateColor = () => {
+    this.setState({
+      color: ColorAppStore.getActiveColor,
+    });
+  };
+
+  render() {
     <div className="Box">
-      <div className="color-container"></div>
-    </div>
-  );
+      <div
+        className="color-container"
+        style={{ backgroundColor: this.state.color }}
+      ></div>
+    </div>;
+  }
 }
 
 export default Box;
