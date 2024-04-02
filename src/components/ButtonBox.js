80744d68ba35e22e3dd55364d4244f6612a27966
Steven <stevengf14@gmail.com>
2024-04-02 13:14:23 -0500

    structuring code

--- src/components/ButtonBox.js
+++ src/components/ButtonBox.js
@@ -1,12 +1,15 @@
+import React from "react";
 import "./ButtonBox.css";
-import * as ColorAppActions from "../actions/ColorappActions";
-import colorappStore from "../stores/ColorappStore";
+import * as ColorAppActions from "../actions/ColorAppActions";
 
 function ButtonBox() {
+  const buttonClick = (color) => {
+    ColorAppActions.changeColor(color);
+  };
   return (
     <div className="ButtonBox">
-      <button>Red</button>
-      <button>Green</button>
+      <button onClick={(event) => buttonClick("#ff0000")}>Red</button>
+      <button onClick={(event) => buttonClick("#00ff00")}>Green</button>
     </div>
   );
 }
--- src/stores/ColorappStore.js
+++ src/stores/ColorappStore.js
@@ -1,15 +1,30 @@
 import dispatcher from "../Dispatcher";
-import * as ColorAppActions from "../actions/ColorappActions";
+import * as ColorAppActions from "../actions/ColorAppActions";
 import { EventEmmiter } from "events";
 
-class ColorappStore extends EventEmmiter {
+class ColorAppStore extends EventEmmiter {
   constructor() {
     super();
+
+    this.activeColor = "#ff0000";
   }
-  handleActions(action) {}
+  handleActions(action) {
+    switch (action.type) {
+      case ColorAppActions.COLOR_APP_ACTIONS.CHANGE_COLOR: {
+        this.activeColor = action.value;
+        this.emmit("storeUpdated");
+        break;
+      }
+      default: {
+      }
+    }
+  }
+  getActiveColor() {
+    return this.activeColor;
+  }
 }
 
-const colorAppStore = new ColorappStore();
+const colorAppStore = new ColorAppStore();
 dispatcher.register(colorAppStore.handleActions.bind(colorAppStore));
 
 export default colorAppStore;
